02b00bc5882e944b7ab156dfe9ad2af481c665dd
Sascha Hemleb <sascha.hemleb@eventim.de>
2024-01-22 23:26:02 +0100

    wip

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,3 +4,5 @@ if 1>/dev/null 2>&1 which brew; then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias pubkey="more $HOME/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
