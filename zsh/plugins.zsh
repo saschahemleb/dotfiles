dbde602ca93139e9340c4ffaabddc1d096527d2b
Sascha Hemleb <sascha.hemleb@eventim.de>
2024-01-23 09:32:04 +0100

    wip

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,7 @@
 zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
 if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
 (
-    . /usr/local/opt/antidote/share/antidote/antidote.zsh
+    . /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
     antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
 )
 fi
