c7fe39f32263986ea99cc5d3cea3d2264fd71de1
Sascha Hemleb <sascha.hemleb@eventim.de>
2025-09-22 07:51:14 +0200

    wip

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,8 @@
 alias dc='docker compose'
 alias dori='docker run -it --rm '
+alias dorime='docker run -it --rm --volume ./:/workdir --volume $SSH_AUTH_SOCK:$SSH_AUTH_SOCK -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK --workdir /workdir --entrypoint /bin/bash'
 
-alias watchdog='docker run --env WATCHDOG_USERNAME --env WATCHDOG_PASSWORD --interactive --volume $(pwd):/a --workdir /a nexus.eventim.net:5043/tixx/watchdog/watchdog-cli:1.0.0'
+alias watchdog='docker run --platform linux/amd64 --rm --env WATCHDOG_USERNAME --env WATCHDOG_PASSWORD --interactive --volume $(pwd):/a --workdir /a harbor.eventim.net/tixx/tixx-all/watchdog/watchdog-cli:1.0.6'
 
 function netshoot_docker_container() {
 	if [ -z "$1" ]; then
@@ -15,4 +16,16 @@ alias netshoot_docker_host='docker run -it --net host nicolaka/netshoot'
 
 function k6() {
     docker run --rm --interactive --volume $PWD:/app --workdir /app grafana/k6:master-with-browser $@
+}
+
+function php@8.1() {
+    docker run --rm --interactive --volume $PWD:/_ --workdir /_ harbor.eventim.net/tixx/tixx-all/docker/php:8.1 $@
+}
+
+function php@8.2() {
+    docker run --rm --interactive --volume $PWD:/_ --workdir /_ harbor.eventim.net/tixx/tixx-all/docker/php:8.2 $@
+}
+
+function php@8.3() {
+    docker run --rm --interactive --volume $PWD:/_ --workdir /_ harbor.eventim.net/tixx/tixx-all/docker/php:8.3 $@
 }
