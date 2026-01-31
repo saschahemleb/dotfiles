577062f552415c989d62671214051711335cf8fb
Sascha Hemleb <sascha@hemleb.de>
2026-01-31 08:46:18 +0100

    wip

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,3 +5,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' insert-tab pending
 
 bindkey -M menuselect '^M' .accept-line
+
+# This makes ← and → always move the cursor on the command line
+bindkey -M menuselect  '^[[D' .backward-char  '^[OD' .backward-char
+bindkey -M menuselect  '^[[C'  .forward-char  '^[OC'  .forward-char
+
