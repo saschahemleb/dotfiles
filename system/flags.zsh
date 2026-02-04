039b5a167bf10d85b82d2901e72cb8d736000605
Sascha Hemleb <sascha@hemleb.de>
2026-02-04 14:57:22 +0100

    wip

--- system/flags.zsh
+++ system/flags.zsh
@@ -1,3 +1,6 @@
-export CFLAGS="-I$(brew --prefix)/include"
-LDFLAGS="-L$(brew --prefix)/lib"
-export CPPFLAGS=$CFLAGS
+local os=$(dotfiles-os)
+if [[ "$os" == "macos" ]]; then
+  export CFLAGS="-I$(brew --prefix)/include"
+  LDFLAGS="-L$(brew --prefix)/lib"
+  export CPPFLAGS=$CFLAGS
+fi
